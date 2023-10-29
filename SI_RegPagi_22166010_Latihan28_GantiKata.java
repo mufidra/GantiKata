b421b4c70f831b6541094f40c5ee19834ea02b94
mufidra <149283162+mufidra@users.noreply.github.com>
2023-10-29 17:28:42 +0700

    Add files via upload

---
+++ SI_RegPagi_22166010_Latihan28_GantiKata.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_22166010_latihan28_gantikata;
+import java.util.Scanner;
+/**
+ *
+ * @author ACER
+ * NAMA : Mufida Anggradita
+ * KELAS : PBO28
+ * NIM : 22166010
+ * DESKRIPSI PROGRAM : Ganti Kata
+ */
+public class SI_RegPagi_22166010_Latihan28_GantiKata {
+
+    public static void main(String[] args) {
+        
+        System.out.println("=======  Program Mengganti Kata =======");
+        Scanner input = new Scanner(System.in);
+        String sentence, oldWord, newWord;
+
+        System.out.print("Masukkan kalimat: ");
+        sentence = input.nextLine();
+
+        System.out.print("Ganti Kata: ");
+        oldWord = input.nextLine();
+
+        System.out.print("Menjadi Kata: ");
+        newWord = input.nextLine();
+        
+        System.out.println("======= Hasil Formatting =======");
+        String oldSentence = sentence.replace(oldWord, newWord);
+        String newSentence = sentence.replace(oldWord, newWord);
+        System.out.println("Kalimat awal: " + oldSentence);
+        System.out.println("Kalimat baru: " + newSentence);
+    
+    }
+}
